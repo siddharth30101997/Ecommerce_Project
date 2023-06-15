fee358bcd8463dc7622c875a7752f93aaa80ab1d
atharv4599 <athu4599@gmail.com>
2023-06-15 10:01:17 +0530

    123

--- Ecommerce_Website_Project/src/main/java/com/ecommerce/main/EcommerceWebsiteProjectApplication.java
+++ Ecommerce_Website_Project/src/main/java/com/ecommerce/main/EcommerceWebsiteProjectApplication.java
@@ -2,12 +2,16 @@ package com.ecommerce.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Producer;
+
+import com.ecommerce.main.model.Products;
 
 @SpringBootApplication
 public class EcommerceWebsiteProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceWebsiteProjectApplication.class, args);
+		
 	}
 
 }
